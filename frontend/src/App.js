7d6e3aeafb473e5b6737ccedf5ed95d656c9b68c
ayuubb <ayubbkj2@gmail.com>
2022-04-18 15:16:01 +0800

    FE firts commit

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import UserList from 'components/UserList';
+
+function App() {
+  return (
+    <div className="container">
+      <UserList></UserList>
+    </div>
+  );
+}
+
+export default App;
